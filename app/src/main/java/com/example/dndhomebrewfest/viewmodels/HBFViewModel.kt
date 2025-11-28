38aca0e009a6682cb9329dda4e33d5075c0e1caa
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-11-28 15:08:04 -0500

    Refactored HBF ViewModel

--- app/src/main/java/com/example/dndhomebrewfest/viewmodels/HBFViewModel.kt
+++ app/src/main/java/com/example/dndhomebrewfest/viewmodels/HBFViewModel.kt
@@ -1,6 +1,7 @@
-package com.example.dndhomebrewfest
+package com.example.dndhomebrewfest.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.dndhomebrewfest.HBFUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
