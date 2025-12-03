0ecf4cf4fe4784b9fc2413d36277f3068bc77e65
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-12-02 23:03:59 -0500

    Added top bar

--- app/src/main/java/com/example/dndhomebrewfest/MainActivity.kt
+++ app/src/main/java/com/example/dndhomebrewfest/MainActivity.kt
@@ -1,39 +1,37 @@
 package com.example.dndhomebrewfest
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -43,7 +41,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.dndhomebrewfest.ui.theme.DnDHomebrewfestTheme
-import com.example.dndhomebrewfest.viewmodels.DnDViewModel
 import com.example.dndhomebrewfest.viewmodels.RoomVM
 import com.example.dndhomebrewfest.data.Character
 import com.example.dndhomebrewfest.screens.StandardSearchScreen
@@ -72,21 +69,54 @@ class MainActivity : ComponentActivity() {
 
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     topBar = {
-
+                        TopBar(screenName, navController::navigateUp)
                     },
                     bottomBar = {
                         BottomBar(screenName = screenName, navigate = navController::navigate)
-                    }) { innerPadding ->
-
-                    Homebrewery(navController)
+                    }
+                ) { innerPadding ->
+                        Box(modifier = Modifier.padding(innerPadding)) {
+                            Homebrewery(navController)
+                        }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(modifier: Modifier = Modifier, screenName : String, back : () -> Unit) {
+fun TopBar(screenName : String, back : () -> Unit, modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            Row(
+                modifier = modifier.fillMaxWidth()
+                    .height(100.dp)
+                    .padding(start = 10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = back
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                        contentDescription = null
+                    )
+                }
+
+                Spacer(modifier = modifier.width(40.dp))
+
+                Text("Homebrewery",
+                    style = MaterialTheme.typography.displaySmall)
+
+
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    )
 
     // back button
 
@@ -180,15 +210,6 @@ fun Homebrewery(navController : NavHostController, modifier: Modifier = Modifier
         composable(route = Screens.StandardSearch.name) {
             StandardSearchScreen(navController, hbfVM)
         }
-    }
-}
-
-@Composable
-fun dbCard(character : Character, modifier: Modifier = Modifier) {
-
-    Card(modifier = modifier.fillMaxWidth()) {
-        Text(character.name)
-        Text(character.character_class)
     }
 }
 
